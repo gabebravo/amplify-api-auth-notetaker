4d6961462b395ab6e98506fbbee03eec3a7aa408
Gabe Bravo <gabecbravo@gmail.com>
2019-02-06 10:39:47 -0500

    Added the Tachyons lib in the index.html file to connect the library

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="flex flex-column items-center justify-center pa3" style={{ background: 'wheat' }}>
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-l">Amplify Note Taker</h1>
         <form className="mb3">
           <input type="text" className="pa2 f4" placeholder="write you note" />
@@ -20,7 +20,7 @@ class App extends Component {
         <div>
           { this.state.notes.map( item => 
             <div key={item.id} className="flex items-center">
-              <li className="list pal f3">{`${item.note}`}</li>
+              <li className="list pa1t f3">{`${item.note}`}</li>
               <button className="bg-transparent bn f4"><span>&times;</span></button>
             </div>
           )}
