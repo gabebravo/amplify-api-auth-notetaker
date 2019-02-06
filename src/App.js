7d7da5c2661c7eb0294ec6cd42b1a953526444d1
Gabe Bravo <gabecbravo@gmail.com>
2019-02-06 00:15:08 -0500

    Added basic UI

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import { withAuthenticator } from 'aws-amplify-react';
 
 class App extends Component {
+
+  state = {
+    notes: [
+      { id: 1, note: 'hello world'}
+    ]
+  }
+
   render() {
     return (
-      <div className="App">
-        App
+      <div className="flex flex-column items-center justify-center pa3" style={{ background: 'wheat' }}>
+        <h1 className="code f2-l">Amplify Note Taker</h1>
+        <form className="mb3">
+          <input type="text" className="pa2 f4" placeholder="write you note" />
+          <button type="submit" className="pa2 f4">Add Note</button>
+        </form>
+        <div>
+          { this.state.notes.map( item => 
+            <div key={item.id} className="flex items-center">
+              <li className="list pal f3">{`${item.note}`}</li>
+              <button className="bg-transparent bn f4"><span>&times;</span></button>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
