919df5a7de94a41f3ebd4449b9c3bce8b97638a3
Gabe Bravo <gabecbravo@gmail.com>
2019-02-09 00:01:54 -0500

    Added react 16.8 and a local state hook. Also added the amplify graphqlOperation for the createNote mutation

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { withAuthenticator } from 'aws-amplify-react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { createNote } from './graphql/mutations';
 
-class App extends Component {
+export default function App() {
 
-  state = {
+  const [state, setState] = useState({
+    note: '',
     notes: [
-      { id: 1, note: 'hello world'}
     ]
+  })
+
+  function handleChange(ev){
+    ev.preventDefault()
+    API.graphql(graphqlOperation(createNote, { 
+      input: { note: state.note }
+    }))
   }
 
-  render() {
-    return (
-      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
-        <h1 className="code f2-l">Amplify Note Taker</h1>
-        <form className="mb3">
-          <input type="text" className="pa2 f4" placeholder="write you note" />
-          <button type="submit" className="pa2 f4">Add Note</button>
-        </form>
-        <div>
-          { this.state.notes.map( item => 
-            <div key={item.id} className="flex items-center">
-              <li className="list pa1t f3">{`${item.note}`}</li>
-              <button className="bg-transparent bn f4"><span>&times;</span></button>
-            </div>
-          )}
-        </div>
+  return (
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-l">Amplify Note Taker</h1>
+      <form className="mb3" onSubmit={handleChange}>
+        <input type="text" value={state.note} className="pa2 f4" placeholder="write you note" onChange={ e => setState({ ...state, note: e.target.value }) }/>
+        <button type="submit" className="pa2 f4">Add Note</button>
+      </form>
+      <div>
+        {/* { this.state.notes.map( item => 
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1t f3">{`${item.note}`}</li>
+            <button className="bg-transparent bn f4"><span>&times;</span></button>
+          </div>
+        )} */}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default withAuthenticator(App, { includeGreetings:  true });
+withAuthenticator(App, { includeGreetings:  true });
