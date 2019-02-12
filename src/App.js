c3e67bc6d67495b767402ad4f99a0f1542af1cec
Gabe Bravo <gabecbravo@gmail.com>
2019-02-11 23:18:34 -0500

    Wrote the delete handler for the delete API call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { withAuthenticator } from 'aws-amplify-react';
 import { API, graphqlOperation } from 'aws-amplify';
-import { createNote } from './graphql/mutations';
+import { createNote, deleteNote } from './graphql/mutations';
 import { listNotes } from './graphql/queries';
 
 export default function App() {
@@ -25,6 +25,15 @@ export default function App() {
     setState({ note: '', notes: [...state.notes, newNote ] })
   }
 
+  async function handleDelete(noteId){
+    const res = await API.graphql(graphqlOperation(deleteNote, { 
+      input: { id: noteId }
+    }))
+    const deletedNoteId = res.data.deleteNote.id;
+    const newNotes = [...state.notes].filter( note => note.id !== deletedNoteId ) ;
+    setState({ note: '', notes: newNotes })
+  }
+
   return (
     <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
       <h1 className="code f2-l">Amplify Note Taker</h1>
@@ -36,7 +45,7 @@ export default function App() {
         { state.notes.map( item => 
           <div key={item.id} className="flex items-center">
             <li className="list pa1t f3">{`${item.note}`}</li>
-            <button className="bg-transparent bn f4"><span>&times;</span></button>
+            <button className="bg-transparent bn f4" onClick={() => handleDelete(item.id)}><span>&times;</span></button>
           </div>
         )}
       </div>
